f6e9a3c44c3e6b9f8b461ae65c4845993fdabce4
AntonKurinnoy <anton.kurinnoy@gmail.com>
2023-10-02 17:26:50 +0300

    update tflite

--- tflite.py
+++ tflite.py
@@ -1,8 +1,16 @@
 import tensorflow as tf
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--saved_model_dir", type=str, default=None, help="path to saved model dir")
+parser.add_argument("-f", "--tflite_file_path", type=str, default='model.tflite', help="path to 'tflite' file")
 
 if __name__ == '__main__':
-    converter = tf.lite.TFLiteConverter.from_saved_model('model/saved_model_dir')
+    args = vars(parser.parse_args())
+
+    saved_model_dir = args["saved_model_dir"]
+    tflite_file = args["tflite_file_path"]
+    converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
     tflite_model = converter.convert()
-    tflite_model_path = 'model/model.tflite'
-    tf.io.write_file(tflite_model_path, tflite_model)
+    tf.io.write_file(tflite_file, tflite_model)
